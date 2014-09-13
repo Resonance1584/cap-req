18f9258062390641a1d2ccce795eb78f9d970174
Lewis Christie <lewis.christie@gmail.com>
2014-09-13 12:09:03 +0200

    add simple express server logging all requests

---
+++ index.js
@@ -0,0 +1,37 @@
+/*jshint node:true*/
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 3999;
+
+app.use (function(req, res, next) {
+    var data='';
+    req.setEncoding('utf8');
+    req.on('data', function(chunk) { 
+       data += chunk;
+    });
+
+    req.on('end', function() {
+        req.body = data;
+        next();
+    });
+});
+
+app.all('/', function (req, res) {
+  console.log('\n');
+  console.log((new Date()).toISOString());
+  console.log('\n');
+  console.log(req.headers);
+  console.log(req.body);
+  res.status(200).end();
+  console.log('\n');
+  console.log('---');
+
+});
+
+app.listen(port, function () {
+  console.log('Listening on port %d', port);
+});
